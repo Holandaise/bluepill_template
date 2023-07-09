090b81fc12478256d6f540b5f219f74a4506bedd
Holandaise <adamlmontgomery@gmail.com>
2023-07-09 13:32:32 -0400

    RCC enable GPIOA clock aswell as RCC enable USART1

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,7 +1,10 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
+
+#define BAUDRATE 115200
 
 static void clock_setup(void){
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
@@ -9,7 +12,8 @@ static void clock_setup(void){
 
 static void gpio_setup(void){
 	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 }
 
 static void systick_setup(void){
@@ -18,6 +22,26 @@ static void systick_setup(void){
 	systick_set_reload(rcc_ahb_frequency / 1000 -1); // reload systick every millisecond
 	systick_interrupt_enable();
 	systick_counter_enable();
+}
+
+static void uart_setup(void){
+	rcc_periph_clock_enable(RCC_USART1);
+	// setup gpio pins
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX|GPIO_USART1_RX);
+	// set baudrate
+	usart_set_baudrate(USART1, BAUDRATE);
+	// set databits
+	usart_set_databits(USART1, 8);
+	// set stop bits
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	// set mode
+	usart_set_mode(USART1, USART_MODE_TX_RX);
+	// set parity
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	// set flow control
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	// enable usart
+	usart_enable(USART1);
 }
 
 static volatile uint64_t _millis = 0;
@@ -32,12 +56,26 @@ static void delay(uint32_t ms){
 }
 
 int main(void){
+
+	int i, j= 0, c= 0;
 	clock_setup();
 	gpio_setup();
 	systick_setup();
+	uart_setup();
+
+
 	while(1){
 		gpio_toggle(GPIOC, GPIO13);
-		delay(500);
+		usart_wait_send_ready(USART1);
+		usart_send_blocking(USART1, c + '0');
+		c = (c == 9) ? 0 : c + 1;
+		if((j++ % 80) == 0){
+			usart_wait_send_ready(USART1);
+			usart_send_blocking(USART1, '\r');
+			usart_wait_send_ready(USART1);
+			usart_send_blocking(USART1, '\n');
+		}
+		delay(100);
 	}
 	return 0;
 }
