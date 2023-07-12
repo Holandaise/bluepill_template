98e665c527aa0ce1f2e82cf3f9173ca053024dbd
Holandaise <adamlmontgomery@gmail.com>
2023-07-12 12:21:09 -0400

    Too many changes to list
    Gotta get better at committing more frequently.

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,82 +1,76 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/i2c.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 
+#include <stdio.h>
+#include <common/setup.h>
+
 #define BAUDRATE 115200
+#define ENC_ADDR 0x36
+#define RAW_ANGLE_ADDRH 0x0E
+#define RAW_ANGLE_ADDRL 0x0F
 
-static void clock_setup(void){
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
-}
 
-static void gpio_setup(void){
-	rcc_periph_clock_enable(RCC_GPIOC);
-	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-}
-
-static void systick_setup(void){
+static void systick_setup(void)
+{
 	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
 	systick_set_frequency(1000, rcc_ahb_frequency); // this calls systick_set_reload
-	//systick_set_reload(rcc_ahb_frequency / 1000 -1); // reload systick every millisecond
 	systick_clear();
 	systick_interrupt_enable();
 	systick_counter_enable();
 }
 
-static void uart_setup(void){
-	rcc_periph_clock_enable(RCC_USART1);
-	// setup gpio pins
-	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX|GPIO_USART1_RX);
-	// set baudrate
-	usart_set_baudrate(USART1, BAUDRATE);
-	// set databits
-	usart_set_databits(USART1, 8);
-	// set stop bits
-	usart_set_stopbits(USART1, USART_STOPBITS_1);
-	// set mode
-	usart_set_mode(USART1, USART_MODE_TX_RX);
-	// set parity
-	usart_set_parity(USART1, USART_PARITY_NONE);
-	// set flow control
-	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-	// enable usart
-	usart_enable(USART1);
-}
-
 static volatile uint64_t _millis = 0;
 
-void sys_tick_handler(void){
+void sys_tick_handler(void)
+{
 	_millis++;
 }
 
-static void delay(uint32_t ms){
+static void delay(uint32_t ms)
+{
 	const uint64_t until = _millis + ms;
-	while(_millis < until);
+	while (_millis < until)
+		;
 }
 
-int main(void){
 
-	int i, j= 0, c= 0;
+int main(void)
+{
+
+	int i, j = 0, c = 0;
 	clock_setup();
-	gpio_setup();
 	systick_setup();
 	uart_setup();
+	iic_setup();
 
+	usart_wait_send_ready(USART1);
 
-	while(1){
-		gpio_toggle(GPIOC, GPIO13);
-		usart_wait_send_ready(USART1);
-		usart_send_blocking(USART1, c + '0');
-		c = (c == 9) ? 0 : c + 1;
-		if((j++ % 80) == 0){
-			usart_wait_send_ready(USART1);
-			usart_send_blocking(USART1, '\r');
-			usart_wait_send_ready(USART1);
-			usart_send_blocking(USART1, '\n');
-		}
-		delay(1000);
+	uint8_t write_buf;
+	uint8_t read_h = 0, read_l = 0;
+	uint16_t raw_encoder = 0;
+	volatile float encoder_val = 0;
+	char to_send[7];
+	usart_write_string(USART1, "Angle\n");
+	float scale = (360.0f/4096.0f);
+
+	while (1)
+	{
+		write_buf = RAW_ANGLE_ADDRH;
+		//i2c_transfer7(I2C1, ENC_ADDR, &write_buf, 1, (uint8_t*)&raw_encoder, 2);
+		//write_buf = RAW_ANGLE_ADDRL;
+		//i2c_transfer7(I2C1, ENC_ADDR, &write_buf, 1, &read_l, 1);
+		//raw_encoder = (uint16_t)read_h << 8 | (uint16_t)read_l;
+		raw_encoder++;
+		raw_encoder &= 0x0FFF;
+		encoder_val = raw_encoder * scale;
+
+		snprintf(to_send, 7, "%5.2f\n", encoder_val);
+		usart_write_string(USART1, to_send);
+		delay(30);
 	}
 	return 0;
 }
